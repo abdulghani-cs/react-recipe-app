6e75b4956d6e93396ff3999c59d5d00500f0d102
abdulghani-cs <ssamimam9865@gmail.com>
2024-08-07 14:36:34 +0500

    server

---
+++ connection.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const connectDB = async () => {
+  try {
+    const connection = await mongoose.connect(process.env.MONGO_URI);
+    if (connection.STATES.connected) {
+      console.log("MongoDB connected");
+    } else {
+      console.log("MongoDB connection failed");
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+module.exports = { connectDB };
